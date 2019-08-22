e4e79bac2469b4dade60853eb4111d46477b77ae
Javier Sánchez de Lago <JSanchezL@kabel.es>
2019-08-22 13:04:11 +0200

    Improving burger proyect

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -9,13 +9,12 @@ import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Orders extends Component {
     componentDidMount () {
-        this.props.onFetchOrders(this.props.token);
+        this.props.onFetchOrders(this.props.token, this.props.userId);
     }
 
     render () {
         let orders = <Spinner />;
         if ( !this.props.loading ) {
-          console.log(this.props.orders)
           if(this.props.orders.length > 0){
             orders = this.props.orders.map( order => (
               <Order
@@ -39,13 +38,14 @@ const mapStateToProps = state => {
     return {
         orders: state.order.orders,
         loading: state.order.loading,
-        token: state.auth.token
+        token: state.auth.token,
+        userId: state.auth.userId
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onFetchOrders: (token) => dispatch( actions.fetchOrders(token) )
+        onFetchOrders: (token, userId) => dispatch( actions.fetchOrders(token, userId) )
     };
 };
 
--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -59,10 +59,12 @@ export const fetchOrdersStart = () => {
     type: actionTypes.FETCH_ORDERS_START
   };
 };
-export const fetchOrders = token => {
+export const fetchOrders = (token, userId) => {
   return dispatch => {
+    dispatch(fetchOrdersStart());
+    const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
     axios
-      .get("/orders.json?auth=" + token)
+      .get("/orders.json" + queryParams)
       .then(res => {
         const fetchedOrders = [];
         for (let key in res.data) {
